dbc7ffecd65d962a92f812466874137c537f7436
Hakumai <inside.rice164@gmail.com>
2019-04-16 22:17:19 +0900

    Introduce normalize.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const { src, dest, series, parallel } = require('gulp');
+const { src, dest, watch, task, series, parallel } = require('gulp');
 const sass = require("gulp-sass");
 const ts = require('gulp-typescript');
 
@@ -14,14 +14,30 @@ function css() {
     .pipe(dest("dist/css"));
 }
 
+function lib_css() {
+  return src("node_modules/normalize.css/normalize.css")
+    .pipe(dest("css"));
+}
+
 function js() {
   return src('src/scripts/main.ts', { sourcemaps: true })
     .pipe(ts())
     .pipe(dest('dist/js'));
 }
 
+task('watch', function() {
+  watch(
+    [
+      'src/scripts/*.ts',
+      'src/stylesheets/*.scss',
+      'index.html'
+    ], parallel(js, css, lib_css)
+  )
+});
+
 exports.js = js;
 exports.css = css;
+exports.lib_css = lib_css;
 
-const build = parallel(css, js);
+const build = parallel(css, js, lib_css);
 exports.default = series(clean, build);
