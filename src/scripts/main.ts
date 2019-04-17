6b3ee723b5b431f885331129da8c7996ff7632ae
Hakumai <inside.rice164@gmail.com>
2019-04-18 01:53:10 +0900

    Implement a function to switch language

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -7,23 +7,59 @@ UIkit.use(Icons);
 
 require("node_modules/jquery-inview/jquery.inview.js");
 
+type langType = "ja" | "en";
+
+let currentLang:langType = "ja";
+
+function switchLang(lang: langType) {
+   currentLang = lang;
+   const counterLang = lang === "ja" ? "en" : "ja";
+   $(`#language-control button[data-lang='${lang}']`).addClass("button-active");
+   $(`#language-control button[data-lang='${counterLang}']`).removeClass("button-active");
+
+   $('h1, h2').each(function() {
+      if ($(this).hasClass("active")) {
+         const shuffleText = new ShuffleText(this);
+         const output = $(this).attr(`data-${lang}-text`);
+         shuffleText.setText("     " + output + "     ");
+         shuffleText.start();
+      }
+   });
+}
+
 $(() => {
 
    $('h1, h2').on('inview', function (event, isInView) {
       if (isInView && !$(this).hasClass("active")) {
             const shuffleText = new ShuffleText(this);
-            const output = $(this).attr("data-ja-text");
+            const targetLang = currentLang;
+            const output = $(this).attr(`data-${targetLang}-text`);
 
             if (output) {
                $(this).text("　").addClass("active");
                shuffleText.setText("     " + output + "     ");
                shuffleText.start();
             } else {
-               throw Error('Attribute "data-ja-text" is not defined');
+               throw Error(`Attribute "data-${targetLang}-text" is not defined`);
             }
       } else {
          // do nothing
       }
    });
+
+   $("#language-control button").on("click", function() {
+      if (!$(this).hasClass("button-active")) {
+         const nextLang =  $(this).attr("data-lang");
+         if (nextLang) {
+            if (nextLang === "ja" || nextLang === "en") {
+               switchLang(nextLang);
+            } else {
+               throw Error('Invalid value of "data-lang" attribute: ' + nextLang);
+            }
+         } else {
+            throw Error('Attribute "data-lang" is not defined');
+         }
+      }
+   })
 
 });
